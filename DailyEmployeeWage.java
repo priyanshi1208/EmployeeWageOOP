23e4ae2365fcbf0d970777c1e3ddffac2bb94023
priyanshi1208 <pri.shukla2208@gmail.com>
2021-05-20 16:58:48 +0530

    added daily wage calculation file

---
+++ DailyEmployeeWage.java
@@ -0,0 +1,8 @@
+package com.employeeWage;
+
+public class DailyEmployeeWage {
+	 	public int DailyWage(int workingHour, int wagePerHour){
+		int wagePerDay=workingHour*wagePerHour;
+		return wagePerDay;
+	}
+}
