3f2b7d092affd469c33a9e59090af859a2829e7c
priyanshi1208 <pri.shukla2208@gmail.com>
2021-05-20 17:31:30 +0530

    deleted file

--- EmployeeWage.java
+++
@@ -1,9 +0,0 @@
-package com.employeeWage;
-
-public class EmployeeWage {
-	public static void main(String[] args) {
-		MonthlyWageCondition monthlyWage=new MonthlyWageCondition();
-		int wage=monthlyWage.EnsuringCondition(100,20,0,0,20);
-		System.out.println(wage);
-	}
-}
